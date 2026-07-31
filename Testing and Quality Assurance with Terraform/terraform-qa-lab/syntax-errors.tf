a6cb342c383ebad714bca9e25957e11e931c5afe
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-07-30 18:35:01 -0700

    Update syntax-errors.tf

--- Testing and Quality Assurance with Terraform/terraform-qa-lab/syntax-errors.tf
+++ Testing and Quality Assurance with Terraform/terraform-qa-lab/syntax-errors.tf
@@ -22,3 +22,21 @@ resource "broken_resource" {
 output "broken_output" {
   value = ${aws_instance.broken_instance.id}  # Old syntax
 }
+
+
+# Fixed syntax errors
+
+resource "aws_instance" "fixed_instance" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  
+  # Fixed closing quote
+  tags = {
+    Name = "fixed-instance"
+  }
+}
+
+# Fixed output with correct syntax
+output "fixed_output" {
+  value = aws_instance.fixed_instance.id
+}
