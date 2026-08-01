f09a4906e7caeb8be6352838e8e0c6d1c691bd2f
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-07-31 18:17:59 -0700

    Create variables.tf

---
+++ Managing Resources with Terraform CLI/terraform-lab16/variables.tf
@@ -0,0 +1,11 @@
+variable "app_version" {
+  description = "Application version"
+  type        = string
+  default     = "1.0.0"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "development"
+}
