27313b9a916ad44a7c6d75692184e8618a492497
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-07-31 21:30:57 -0700

    Add variable for creating multiple config files

--- Advanced Terraform Troubleshooting/terraform-troubleshooting-lab/scenario3-error-handling/main.tf
+++ Advanced Terraform Troubleshooting/terraform-troubleshooting-lab/scenario3-error-handling/main.tf
@@ -42,3 +42,42 @@ resource "local_file" "system_file" {
   filename = "/etc/terraform-lab-test.conf"
   content  = "this will fail without root permissions"
 }
+
+
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
+}
+
+variable "create_files" {
+  description = "List of files to create"
+  type = list(object({
+    name    = string
+    content = string
+    path    = string
+  }))
+  default = [
+    {
+      name    = "app-config"
+      content = "app_name=myapp\nversion=1.0"
+      path    = "config"
+    },
+    {
+      name    = "database-config"
+      content = "db_host=localhost\ndb_port=5432"
+      path    = "config"
+    }
+  ]
+}
+
+resource "local_file" "config_files" {
+  for_each = { for f in var.create_files : f.name => f }
+
+  filename = "${each.value.path}/${each.value.name}.conf"
+  content  = each.value.content
+}
+
